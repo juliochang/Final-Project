b30076fc86bfb1b85ba3dcc90954d2b5bba0ffd2
juliochang <juliochang@hotmail.com>
2016-11-07 17:26:24 -0500

    Final Project

---
+++ infinite-rotator.js
@@ -0,0 +1,66 @@
+$(window).load(function() {	//start after HTML, images have loaded
+
+	var InfiniteRotator = 
+	{
+		init: function()
+		{
+			//initial fade-in time (in milliseconds)
+			var initialFadeIn = 800;
+			
+			//interval between items (in milliseconds)
+			var itemInterval = 3000;
+			
+			//cross-fade time (in milliseconds)
+			var fadeTime = 1500;
+			
+			//count number of items
+			var numberOfItems = $('.rotating-item').length;
+
+			//set current item
+			var currentItem = 0;
+
+			//show first item
+			$('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+
+			//loop through the items		
+			var infiniteLoop = setInterval(function(){
+				$('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+
+				if(currentItem == numberOfItems -1){
+					currentItem = 0;
+				}else{
+					currentItem++;
+				}
+				$('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+
+			}, itemInterval);	
+		}	
+	};
+
+	InfiniteRotator.init();
+	
+});
+
+// number of drops created.
+var nbDrop = 858; 
+
+// function to generate a random number range.
+function randRange( minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
+
+// function to generate drops
+function createRain() {
+
+	for( i=1;i<nbDrop;i++) {
+	var dropLeft = randRange(0,1600);
+	var dropTop = randRange(-1000,1400);
+
+	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+	$('#drop'+i).css('left',dropLeft);
+	$('#drop'+i).css('top',dropTop);
+	}
+
+}
+// Make it rain
+createRain();
